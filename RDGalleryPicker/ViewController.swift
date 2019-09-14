45c069a790bc0149cf7ffa9c8f983fa8b82a83ef
Deniz <mersinliogludeniz@gmail.com>
2019-09-14 15:09:19 +0300

    Agrume init

--- RDGalleryPicker/ViewController.swift
+++ RDGalleryPicker/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController, GalleryControllerDelegate {
         
         button = UIButton(type: .system)
         button.frame.size = CGSize(width: 200, height: 50)
-        button.setTitle("Open Gallery", for: UIControl.State())
+        button.setTitle("Open RDGallery", for: UIControl.State())
         button.addTarget(self, action: #selector(buttonTouched(_:)), for: .touchUpInside)
         
         view.addSubview(button)
@@ -33,23 +33,15 @@ class ViewController: UIViewController, GalleryControllerDelegate {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
         button.center = CGPoint(x: view.bounds.size.width/2, y: view.bounds.size.height/2)
     }
     
     @objc func buttonTouched(_ button: UIButton) {
         gallery = GalleryController()
         gallery.delegate = self
-        
         present(gallery, animated: true, completion: nil)
     }
-    
-    // MARK: - LightboxControllerDismissalDelegate
-    
-//    func lightboxControllerWillDismiss(_ controller: LightboxController) {
-//
-//    }
-    
+
     // MARK: - GalleryControllerDelegate
     
     func galleryControllerDidCancel(_ controller: GalleryController) {
@@ -60,14 +52,11 @@ class ViewController: UIViewController, GalleryControllerDelegate {
     func galleryController(_ controller: GalleryController, didSelectVideo video: Video) {
         controller.dismiss(animated: true, completion: nil)
         gallery = nil
-        
-        
         editor.edit(video: video) { (editedVideo: Video?, tempPath: URL?) in
             DispatchQueue.main.async {
                 if let tempPath = tempPath {
                     let controller = AVPlayerViewController()
                     controller.player = AVPlayer(url: tempPath)
-                    
                     self.present(controller, animated: true, completion: nil)
                 }
             }
@@ -80,26 +69,14 @@ class ViewController: UIViewController, GalleryControllerDelegate {
     }
     
     func galleryController(_ controller: GalleryController, requestLightbox images: [Image]) {
-//        LightboxConfig.DeleteButton.enabled = true
-//
-//        SVProgressHUD.show()
-//        Image.resolve(images: images, completion: { [weak self] resolvedImages in
-//            SVProgressHUD.dismiss()
-//            self?.showLightbox(images: resolvedImages.compactMap({ $0 }))
-//        })
-    }
-    
-    // MARK: - Helper
-    
-    func showLightbox(images: [UIImage]) {
-//        guard images.count > 0 else {
-//            return
-//        }
-//
-//        let lightboxImages = images.map({ LightboxImage(image: $0) })
-//        let lightbox = LightboxController(images: lightboxImages, startIndex: 0)
-//        lightbox.dismissalDelegate = self
-//
-//        gallery.present(lightbox, animated: true, completion: nil)
+        //TODO: - SVProgressHUD.show() -> Create a loader to indicate loading process
+        
+        Image.resolve(images: images, completion: { [weak self] resolvedImages in
+            //TODO: - SVProgressHUD.dismiss() -> Dismiss the loader to indicate loading process ends
+            guard let self = self else {return}
+            let compactImages = resolvedImages.compactMap{$0}
+            let agrume = Agrume(images: compactImages)
+            agrume.show(from: self.gallery, color: .black)
+        })
     }
 }
