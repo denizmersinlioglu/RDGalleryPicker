388746a05943738a7f8c6065fc0bdfd636144750
Deniz <mersinliogludeniz@gmail.com>
2019-09-14 14:35:52 +0300

    Initial Commit

---
+++ RDGalleryPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RDGalleryPicker
+//
+//  Created by Deniz Mersinlioğlu on 14.09.2019.
+//  Copyright © 2019 Development House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
